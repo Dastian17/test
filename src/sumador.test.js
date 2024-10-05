191d759ac77d2193ec054fbfab4f5dae635090d4
DayanAguilar <dayan.aguilar.alcocer@gmail.com>
2024-10-04 20:45:20 -0400

    test130

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,9 +1,15 @@
-import sumar from "./sumador.js";
+import {restar, sumar} from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   }); it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
+  });
+  ; it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+  it("deberia sumar dos numeros", () => {
+    expect(restar(3, 2)).toEqual(1);
   });
 });
